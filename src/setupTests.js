baca70f522a0b6a13576472ae5e0c4da2cd021ce
Ryan Walters <rwalters@eb.com>
2018-02-08 14:23:16 -0600

    add environment variables (wip)

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,12 @@
 
+const { BUCKET, CLOUDFRONT_URL, ERROR_DOCUMENT } = require('../config/local.json').handler;
+
+Object.assign(process.env, {
+  BUCKET,
+  CLOUDFRONT_URL,
+  ERROR_DOCUMENT
+});
+
 // Put logging on silent mode
 
 const { logger } = require('./utils');
